760fa5a7d9df7d334b802ad43cf36db26c4602ff
Cris0291 <cris0291@gmail.com>
2025-02-22 22:07:35 -0600

    Modify email form

--- app/ui/EmailForm.tsx
+++ app/ui/EmailForm.tsx
@@ -1,8 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { Send, Mail } from "lucide-react";
+import Image from "next/image";
+import VideoBackground from "./VideoBackground";
+import { cn } from "@/lib/utils";
 
 const EmailForm: React.FC = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  const handleMouseMove = (event: React.MouseEvent<HTMLElement>) => {
+    if (sectionRef.current) {
+      const rect = sectionRef.current.getBoundingClientRect();
+      setMousePosition({
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top,
+      });
+    }
+  };
+
+  useEffect(() => {
+    const handleMouseLeave = () => setIsHovered(false);
+    document.addEventListener("mouseleave", handleMouseLeave);
+    return () => {
+      document.removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, []);
+
+  const size = isHovered ? 400 : 40;
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -47,9 +73,10 @@ const EmailForm: React.FC = () => {
   };
 
   return (
-    <section className="w-full py-5 md:py-10 lg:py-15 xl:py-20">
-      <div className="container mx-auto p-4 bg-gradient-to-br from-blue-100 to-red-100 min-h-screen flex items-center justify-center">
-        <div className="bg-white rounded-lg shadow-2xl overflow-hidden max-w-4xl w-full">
+    <section className="relative w-full py-5 md:py-10 lg:py-15 xl:py-20 overflow-hidden">
+      <VideoBackground videoSrc="/sea.mp4" />
+      <div className="relative z-10 mx-auto p-4 min-h-screen flex items-center justify-center">
+        <div className="bg-white rounded-lg shadow-2xl overflow-hidden w-full sm:max-w-xl md:max-w-4xl">
           <div className="grid md:grid-cols-2">
             <motion.div
               className="p-8"
@@ -57,9 +84,7 @@ const EmailForm: React.FC = () => {
               animate="visible"
               variants={formAnimation}
             >
-              <h2 className="text-3xl font-bold text-blue-800 mb-6">
-                Contact Me
-              </h2>
+              <h2 className="text-3xl font-bold text-black mb-6">Contact Me</h2>
               <form onSubmit={handleSubmit}>
                 {["name", "email", "subject"].map((field) => (
                   <motion.div
@@ -69,7 +94,7 @@ const EmailForm: React.FC = () => {
                   >
                     <label
                       htmlFor={field}
-                      className="block text-gray-700 text-sm font-semibold mb-2 capitalize"
+                      className="block text-[--color-test] text-sm font-semibold mb-2 capitalize"
                     >
                       {field}
                     </label>
@@ -87,7 +112,7 @@ const EmailForm: React.FC = () => {
                 <motion.div className="mb-6" variants={inputAnimation}>
                   <label
                     htmlFor="message"
-                    className="block text-gray-700 text-sm font-semibold mb-2"
+                    className="block text-[--color-test] text-sm font-semibold mb-2"
                   >
                     Message
                   </label>
@@ -103,7 +128,7 @@ const EmailForm: React.FC = () => {
                 <motion.div variants={inputAnimation}>
                   <button
                     type="submit"
-                    className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline transition duration-300 flex items-center"
+                    className="bg-[--color-test] hover:bg-black text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline transition duration-300 flex items-center"
                   >
                     <Send size={18} className="mr-2" />
                     Send Message
@@ -112,10 +137,14 @@ const EmailForm: React.FC = () => {
               </form>
             </motion.div>
             <motion.div
-              className="bg-gradient-to-br from-blue-400 to-red-400 flex items-center justify-center p-8"
+              className="bg-[--color-test] flex items-center justify-center p-8 relative w-full  overflow-hidden"
               initial="hidden"
               animate="visible"
               variants={formAnimation}
+              ref={sectionRef}
+              onMouseMove={handleMouseMove}
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
             >
               <motion.div
                 className="w-40 h-40 bg-white rounded-full flex items-center justify-center shadow-lg"
@@ -123,7 +152,29 @@ const EmailForm: React.FC = () => {
                 animate="animate"
                 variants={envelopeAnimation}
               >
-                <Mail size={64} className="text-blue-600" />
+                <Mail size={64} className="text-[--color-test]" />
+              </motion.div>
+              <motion.div
+                className={cn(
+                  "mask",
+                  "absolute inset-0 pointer-events-none",
+                  "bg-white"
+                )}
+                animate={{
+                  WebkitMaskPosition: `${mousePosition.x - size / 2}px ${
+                    mousePosition.y - size / 2
+                  }px`,
+                  WebkitMaskSize: `${size}px`,
+                }}
+                transition={{ type: "tween", ease: "backOut", duration: 0.2 }}
+              >
+                <Image
+                  src="/back.jpg"
+                  alt="Background"
+                  layout="fill"
+                  objectFit="cover"
+                  quality={100}
+                />
               </motion.div>
             </motion.div>
           </div>
