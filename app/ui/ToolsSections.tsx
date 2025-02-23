2632ddc4f2370a0e695548cb83067ca4f3f9818d
Cris0291 <cris0291@gmail.com>
2025-02-22 22:07:10 -0600

    Modify tools section

--- app/ui/ToolsSections.tsx
+++ app/ui/ToolsSections.tsx
@@ -4,7 +4,7 @@ import TechnologyGrid from "./TechnologyGrid";
 
 export default function ToolsSection() {
   return (
-    <section className="w-full py-5 md:py-10 lg:py-15 xl:py-20">
+    <section className="w-full">
       <TechnologyGrid />
     </section>
   );
